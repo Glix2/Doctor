65273b450a8eac0b8f7aad3176c95c2d3c65dcf4
Glix2 <98677442+Glix2@users.noreply.github.com>
2023-03-12 14:30:45 -0400

    Add files via upload

--- doctor.js
+++ doctor.js
@@ -1,5 +1,15 @@
 const items = document.querySelectorAll('.accordion button');
-
+const galleryItem = document.getElementsByClassName("gallery-item");
+const lightBoxContainer = document.createElement("div");
+const lightBoxContent = document.createElement("div");
+const lightBoxImg = document.createElement("img");
+const lightBoxPrev = document.createElement("div");
+const lightBoxNext = document.createElement("div");
+const toggleButton = document.getElementsByClassName('hamburger')[0]
+const navBarLinks = document.getElementsByClassName('nav-area')[0]
+const navBarLines = document.getElementsByClassName('bar')[0]
+const navBarLinesOne = document.getElementsByClassName('bar1')[0]
+const navBarLinesTwo = document.getElementsByClassName('bar2')[0]
 
 
 window.addEventListener('scroll', revealService);
@@ -28,7 +38,7 @@ window.addEventListener("scroll", function(){
 })
 
 window.addEventListener("scroll", function(){
-  var header = document.querySelector("nav");
+  var header = document.querySelector("navbox");
   header.classList.toggle("stickynav", window.scrollY > 300);
 })
 
@@ -45,3 +55,91 @@ function toggleAccordion() {
 }
 
 items.forEach((item) => item.addEventListener('click', toggleAccordion));
+
+var fullImgBox = document.getElementById("fullImgBox");
+var fullImg = document.getElementById("fullImg");
+
+function openFullImg(pic){
+  fullImgBox.style.display = "grid";
+  fullImg.src = pic;
+}
+function closeFullImg(){
+  fullImgBox.style.display = "none";
+}
+
+
+lightBoxContainer.classList.add("lightbox");
+lightBoxContent.classList.add("lightbox-content");
+lightBoxPrev.classList.add("fa", "fa-angle-left", "lightbox-prev");
+lightBoxNext.classList.add("fa", "fa-angle-right", "lightbox-next");
+
+lightBoxContainer.appendChild(lightBoxContent);
+lightBoxContent.appendChild(lightBoxImg);
+lightBoxContent.appendChild(lightBoxPrev);
+lightBoxContent.appendChild(lightBoxNext);
+document.body.appendChild(lightBoxContainer);
+
+let index = 1;
+
+function showLightBox(n) {
+    if (n > galleryItem.length) {
+        index = 1;
+    } else if (n < 1) {
+        index = galleryItem.length;
+    }
+    let imageLocation = galleryItem[index-1].children[0].getAttribute("src");
+    lightBoxImg.setAttribute("src", imageLocation);
+}
+
+function currentImage() {
+    lightBoxContainer.style.display = "block";
+
+    let imageIndex = parseInt(this.getAttribute("data-index"));
+    showLightBox(index = imageIndex);
+}
+for (let i = 0; i < galleryItem.length; i++) {
+    galleryItem[i].addEventListener("click", currentImage);
+}
+
+function slideImage(n) {
+    showLightBox(index += n);
+}
+function prevImage() {
+    slideImage(-1);
+}
+function nextImage() {
+    slideImage(1);
+}
+lightBoxPrev.addEventListener("click", prevImage);
+lightBoxNext.addEventListener("click", nextImage);
+
+function closeLightBox() {
+    if (this === event.target) {
+        lightBoxContainer.style.display = "none";
+    }
+}
+lightBoxContainer.addEventListener("click", closeLightBox);
+
+
+toggleButton.addEventListener('click', () => {
+  navBarLinks.classList.toggle('active')
+  navBarLines.classList.toggle('active')
+  navBarLinesOne.classList.toggle('active')
+  navBarLinesTwo.classList.toggle('active')
+
+})
+
+// GET ALL LINKS IN NAVBAR
+const links = document.querySelectorAll('.nav-area a')
+
+links.forEach(l => {
+  // BIND CLICK EVENT ON ALL LINKS
+  l.addEventListener('click', () => {
+    // ON CLICK, REMOVE active CLASS FROM navBarLinks
+    navBarLinks.classList.remove('active')
+    navBarLines.classList.remove('active')
+    navBarLinesOne.classList.remove('active')
+    navBarLinesTwo.classList.remove('active')
+
+  })
+})
